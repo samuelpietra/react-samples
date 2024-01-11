f8866ac6a192cc36fe3c706cf73520b9fae269a8
Samuel Pietra <31863795+samuelpietra@users.noreply.github.com>
2024-01-11 17:24:32 -0300

    [Style] Improves SandboxPage presentation (#5)

--- src/pages/Sandbox/Sandbox.tsx
+++ src/pages/Sandbox/Sandbox.tsx
@@ -1,36 +1,26 @@
-import { useState } from 'react'
-
 import reactLogo from '@/assets/react.svg'
+import Column from '@/components/Column'
 import Container from '@/components/Container'
+import Row from '@/components/Row'
 
 import viteLogo from '../../../public/vite.svg'
 
 function SandboxPage() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Container>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} alt="Vite logo" />
-        </a>
+    <Container minHeight="100vh">
+      <Column alignItems="center">
+        <Row>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} alt="Vite logo" />
+          </a>
 
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} alt="React logo" />
-        </a>
-      </div>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} alt="React logo" />
+          </a>
+        </Row>
 
-      <h1>Vite + React</h1>
-
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p>Click on the Vite and React logos to learn more</p>
+        <h1>Vite + React</h1>
+      </Column>
     </Container>
   )
 }
