ef66973d18eacd546d75fa5cb27909eba740f5de
Samuel Pietra <samuel.pietra@localiza.com>
2024-02-06 21:39:10 -0300

    [Feature] Adds react-icons to items at Sidebar

--- src/components/Layout/Sidebar/Sidebar.styles.tsx
+++ src/components/Layout/Sidebar/Sidebar.styles.tsx
@@ -1,4 +1,4 @@
-
+import { FaCode } from 'react-icons/fa'
 import { Link as DefaultLink } from 'react-router-dom'
 
 import styled, { css } from 'styled-components'
@@ -7,30 +7,36 @@ const SIDEBAR_EXPANDED_WIDTH = 248
 const SIDEBAR_MEDIA_WIDTH = 1541
 const SIDEBAR_WIDTH = 72
 
+const Icon = styled(FaCode)<{ selected?: boolean }>`
+  ${({ selected }) => css`
+    color: ${selected ? '#61afef' : '#abb2Bf'};
+  `}
+`
+
 const Link = styled(DefaultLink)<{ selected?: boolean }>`
   ${({ selected }) => css`
     background-color: #282c34;
-    border-bottom: 1px solid ${selected ? '#61AFEF' : '#1d2025'};
+    border-bottom: 1px solid ${selected ? '#61afef' : '#1d2025'};
   `}
   align-items: center;
   display: flex;
-  padding: 16px 24px;
+  padding: 16px;
   text-decoration: none;
 
   &:hover {
     background-color: #2d313b;
 
     h4 {
-      color: ${({ selected }) => !selected && '#FFF'};
+      color: ${({ selected }) => !selected && '#fff'};
     }
   }
 `
 
 const Label = styled.h4<{ selected?: boolean }>`
   ${({ selected }) => css`
-    color: ${selected ? '#61AFEF' : '#ABB2BF'};
+    color: ${selected ? '#61afef' : '#abb2bf'};
   `}
-  margin-left: 8px;
+  margin-left: 12px;
   white-space: nowrap;
 `
 
@@ -54,4 +60,4 @@ const Sidebar = styled.aside`
   z-index: 1;
 `
 
-export { Label, Link, List, Sidebar, SIDEBAR_WIDTH, SIDEBAR_EXPANDED_WIDTH, SIDEBAR_MEDIA_WIDTH }
+export { Icon, Label, Link, List, Sidebar, SIDEBAR_WIDTH, SIDEBAR_EXPANDED_WIDTH, SIDEBAR_MEDIA_WIDTH }
