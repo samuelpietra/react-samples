699203b7a4e049fe04c41cf9d2c53d206f4599fb
Samuel Pietra <samuel.pietra@localiza.com>
2024-02-06 21:38:25 -0300

    [Feature] Adds react-icons and repo link at TopBar

--- src/components/Layout/TopBar/TopBar.styles.tsx
+++ src/components/Layout/TopBar/TopBar.styles.tsx
@@ -3,13 +3,11 @@ import styled from 'styled-components'
 const TOPBAR_HEIGHT = 59
 
 const TopBar = styled.header`
-  align-items: center;
   background-color: #1d2025;
   border-bottom: 1px solid #333;
   display: flex;
-  left: 0px;
-  padding: 8px;
-  right: 0px;
+  justify-content: space-between;
+  padding: 8px 16px;
 `
 
 export { TOPBAR_HEIGHT, TopBar }
--- src/components/Layout/TopBar/TopBar.tsx
+++ src/components/Layout/TopBar/TopBar.tsx
@@ -1,15 +1,20 @@
-import reactLogo from '@/assets/react.svg'
+import { FaReact, FaGithub } from 'react-icons/fa'
 
 import * as S from './TopBar.styles'
+import Row from '@/components/Row'
 
 function TopBar() {
   return (
     <S.TopBar>
-      <a href="https://github.com/samuelpietra/react-samples" target="_blank">
-        <img src={reactLogo} alt="React logo" />
-      </a>
+      <Row alignItems="center">
+        <FaReact color="#61dbfb" size={32} />
 
-      <h2>React Samples</h2>
+        <h2 style={{ marginLeft: 16 }}>React Samples</h2>
+      </Row>
+
+      <a href="https://github.com/samuelpietra/react-samples" target="_blank">
+        <FaGithub color="#ccc" size={32} />
+      </a>
     </S.TopBar>
   )
 }
